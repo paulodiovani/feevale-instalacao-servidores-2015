c46cea92fe9c55619157c90e67eeca107adec277
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-11-07 11:42:17 -0200

    Provision docker install

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo "Hello!"
+
+# Install Docker
+echo "Updating repositories..." &&
+apt-get -qq update &&
+echo "Installing docker..." &&
+apt-get -qq install docker.io > /dev/null &&
+docker --version
