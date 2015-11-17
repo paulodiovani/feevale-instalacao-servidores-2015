c6ac433c80546439936529ec55660b0d2779d6d0
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-11-17 16:31:46 -0200

    Add user to docker group

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -7,3 +7,6 @@ apt-get -qq update &&
 echo "Installing docker..." &&
 apt-get -qq install docker.io > /dev/null &&
 docker --version
+
+# Add vagrant user to docker group
+usermod -a -G docker vagrant
